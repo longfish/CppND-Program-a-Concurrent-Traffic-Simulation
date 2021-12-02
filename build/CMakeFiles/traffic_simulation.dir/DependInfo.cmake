3ed759d6e5e796ccfafca2e3d5021f712ec00e0a
longfish <loongfish@foxmail.com>
2021-12-01 21:48:29 -0700

    done the tasks except FP.2a and FP.6

---
+++ build/CMakeFiles/traffic_simulation.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/d/Code/CCplusplus/Udacity_cpp/4_concurrency/CppND-Program-a-Concurrent-Traffic-Simulation/src/Graphics.cpp" "/mnt/d/Code/CCplusplus/Udacity_cpp/4_concurrency/CppND-Program-a-Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/Graphics.cpp.o"
+  "/mnt/d/Code/CCplusplus/Udacity_cpp/4_concurrency/CppND-Program-a-Concurrent-Traffic-Simulation/src/Intersection.cpp" "/mnt/d/Code/CCplusplus/Udacity_cpp/4_concurrency/CppND-Program-a-Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/Intersection.cpp.o"
+  "/mnt/d/Code/CCplusplus/Udacity_cpp/4_concurrency/CppND-Program-a-Concurrent-Traffic-Simulation/src/Street.cpp" "/mnt/d/Code/CCplusplus/Udacity_cpp/4_concurrency/CppND-Program-a-Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/Street.cpp.o"
+  "/mnt/d/Code/CCplusplus/Udacity_cpp/4_concurrency/CppND-Program-a-Concurrent-Traffic-Simulation/src/TrafficLight.cpp" "/mnt/d/Code/CCplusplus/Udacity_cpp/4_concurrency/CppND-Program-a-Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/TrafficLight.cpp.o"
+  "/mnt/d/Code/CCplusplus/Udacity_cpp/4_concurrency/CppND-Program-a-Concurrent-Traffic-Simulation/src/TrafficObject.cpp" "/mnt/d/Code/CCplusplus/Udacity_cpp/4_concurrency/CppND-Program-a-Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/TrafficObject.cpp.o"
+  "/mnt/d/Code/CCplusplus/Udacity_cpp/4_concurrency/CppND-Program-a-Concurrent-Traffic-Simulation/src/TrafficSimulator-Final.cpp" "/mnt/d/Code/CCplusplus/Udacity_cpp/4_concurrency/CppND-Program-a-Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/TrafficSimulator-Final.cpp.o"
+  "/mnt/d/Code/CCplusplus/Udacity_cpp/4_concurrency/CppND-Program-a-Concurrent-Traffic-Simulation/src/Vehicle.cpp" "/mnt/d/Code/CCplusplus/Udacity_cpp/4_concurrency/CppND-Program-a-Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/Vehicle.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/mnt/d/Ubuntu/apps/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -1,58 +1,95 @@
+#include "TrafficLight.h"
 #include <iostream>
 #include <random>
-#include "TrafficLight.h"
 
 /* Implementation of class "MessageQueue" */
 
-/* 
-template <typename T>
-T MessageQueue<T>::receive()
-{
-    // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
-    // to wait for and receive new messages and pull them from the queue using move semantics. 
-    // The received object should then be returned by the receive function. 
+template <typename T> T MessageQueue<T>::receive() {
+  // FP.5a : The method receive should use std::unique_lock<std::mutex> and
+  // _condition.wait() to wait for and receive new messages and pull them from
+  // the queue using move semantics. The received object should then be returned
+  // by the receive function.
+
+  std::unique_lock<std::mutex> uLock(_mutex);
+  _cond.wait(uLock, [this] { return !_queue.empty(); });
+
+  // remove last vector element from the queue
+  T msg = std::move(_queue.back());
+  _queue.pop_back();
+
+  return msg;
 }
 
-template <typename T>
-void MessageQueue<T>::send(T &&msg)
-{
-    // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
-    // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+template <typename T> void MessageQueue<T>::send(T &&msg) {
+  // FP.4a : The method send should use the mechanisms
+  // std::lock_guard<std::mutex> as well as _condition.notify_one() to add a new
+  // message to the queue and afterwards send a notification.
+  std::lock_guard<std::mutex> uLock(_mutex);
+
+  _queue.push_back(std::move(msg));
+  _cond.notify_one(); // notify client after pushing new Vehicle into vector
 }
-*/
 
 /* Implementation of class "TrafficLight" */
 
-/* 
-TrafficLight::TrafficLight()
-{
-    _currentPhase = TrafficLightPhase::red;
+TrafficLight::TrafficLight() { _currentPhase = TrafficLightPhase::red; }
+
+void TrafficLight::waitForGreen() {
+  // FP.5b : add the implementation of the method waitForGreen, in which an
+  // infinite while-loop runs and repeatedly calls the receive function on the
+  // message queue. Once it receives TrafficLightPhase::green, the method
+  // returns.
+  while (true) {
+    auto msg = _msgQueue->receive();
+    if (msg == TrafficLightPhase::green)
+      break;
+  }
 }
 
-void TrafficLight::waitForGreen()
-{
-    // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
-    // runs and repeatedly calls the receive function on the message queue. 
-    // Once it receives TrafficLightPhase::green, the method returns.
-}
+TrafficLightPhase TrafficLight::getCurrentPhase() { return _currentPhase; }
 
-TrafficLightPhase TrafficLight::getCurrentPhase()
-{
-    return _currentPhase;
-}
-
-void TrafficLight::simulate()
-{
-    // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
+void TrafficLight::simulate() {
+  // FP.2b : Finally, the private method „cycleThroughPhases“ should be started
+  // in a thread when the public method „simulate“ is called. To do this, use
+  // the thread queue in the base class.
+  threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
 }
 
 // virtual function which is executed in a thread
-void TrafficLight::cycleThroughPhases()
-{
-    // FP.2a : Implement the function with an infinite loop that measures the time between two loop cycles 
-    // and toggles the current phase of the traffic light between red and green and sends an update method 
-    // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
-    // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
-}
+void TrafficLight::cycleThroughPhases() {
+  /* FP.2a : Implement the function with an infinite loop that measures the time
+  between two loop cycles and toggles the current phase of the traffic light
+  between red and green and sends an update method to the message queue using
+  move semantics. The cycle duration should be a random value between 4 and 6
+  seconds. Also, the while-loop should use std::this_thread::sleep_for to wait
+  1ms between two cycles. */
 
-*/
+  // pick a random time between 4 and 6 seconds, i.e. [4000, 6000] ms
+  std::random_device rd;
+  std::mt19937 eng(rd());
+  std::uniform_int_distribution<> distr(4000, 6000);
+  double cycleDuration =
+      distr(eng); // duration of a single simulation cycle in ms
+  std::chrono::time_point<std::chrono::system_clock> lastUpdate;
+
+  while (true) {
+    // sleep for 1ms at every iteration
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+    // compute time difference to stop watch
+    long timeSinceLastUpdate =
+        std::chrono::duration_cast<std::chrono::milliseconds>(
+            std::chrono::system_clock::now() - lastUpdate)
+            .count();
+
+    if (timeSinceLastUpdate >= cycleDuration) {
+      // toggle the current phase of the traffic light between red and green and
+      // send this message
+      _currentPhase = TrafficLightPhase::green;
+      //_msgQueue->send(std::move(_currentPhase));
+
+      // reset stop watch for next cycle
+      lastUpdate = std::chrono::system_clock::now();
+    }
+  }
+}
